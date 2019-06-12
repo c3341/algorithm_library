0ff1abbb0a36ed0111af8665a812661cdddeedec
kazuki <masomoi2424@gmail.com>
2019-06-12 20:06:01 +0900

    Add files via upload

---
+++ Graph/Ford-FulkersonAlgorithm.py
@@ -0,0 +1,61 @@
+import sys
+import os
+f = open("C:/Users/kazuki/Documents/python/import.txt","r")
+sys.stdin = f
+# -*- coding: utf-8 -*-
+from itertools import*
+from math import*
+from collections import*
+from heapq import*
+from bisect import bisect_left,bisect_right
+from copy import deepcopy
+inf = float("inf")
+mod = 10**9+7
+from functools import reduce
+import sys
+sys.setrecursionlimit(10**7)
+
+#フォードファルカーソン(O(FE))
+class FordFulkerson:
+    def __init__(self,N):
+        self.N = N
+        self.G = [[] for i in range(N)]
+
+    def add_edge(self,fr,to,cap):
+        forward = [to,cap,None]
+        forward[2] = backward = [fr,0,forward]
+        self.G[fr].append(forward)
+        self.G[to].append(backward)
+
+    def dfs(self,v,t,f):
+        if v == t:
+            return f
+        used = self.used
+        used[v] = 1
+        for e in self.G[v]:
+            w,cap,rev = e
+            if cap and not used[w]:
+                d = self.dfs(w,t,min(f,cap))
+                if d:
+                    e[1] -=d
+                    rev[1] += d
+                    return d
+        return 0
+
+    def flow(self,s,t):
+        flow = 0
+        f = inf
+        N = self.N
+        while f:
+            self.used = [0]*N
+            f = self.dfs(s,t,inf)
+            flow += f
+        return flow
+
+V,E = map(int,input().split())
+ff = FordFulkerson(V)
+for i in range(E):
+    v0,v1,c = map(int,input().split())
+    ff.add_edge(v0,v1,c)
+
+print(ff.flow(0,V-1))
